5e1f20c48981674344ef6e21a4c79fa8db70adbb
rocky1109 <riverdale1109@gmail.com>
2017-06-28 17:21:26 +0530

    Turned of all the external logging requests and urllib3

--- src/pyVirtualize/__init__.py
+++ src/pyVirtualize/__init__.py
@@ -26,5 +26,9 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+import logging
+
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 __all__ = ['pyvSphere', 'vSphere', 'Process', 'WinUtils']
